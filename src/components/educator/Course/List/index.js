4b3d118dea3b6487b03539dc907b53bf9adce4c5
narayanbavisetti <narayan3119@gmail.com>
2022-09-27 08:55:03 +0530

    added the educator course

--- src/components/educator/Course/List/index.js
+++ src/components/educator/Course/List/index.js
@@ -1,39 +1,35 @@
 import axios from "axios";
 import React, { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import authHeader from "../../../features/auth/auth-header";
 import SingleCourse from "./Single/index";
 
 const CourseList = () => {
 
-//   const [person, setPerson] = useState();
-//   useEffect(() => {
-//     if (localStorage.getItem("data") != null) {
-//       let data = JSON.parse(localStorage.getItem("data"));
-//       setPerson(data.personType);
-//     }
-//   }, [person]);
+  const [course, setCourse] = useState([]);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    allCourses();
+  }, []);
 
-// console.log(person)
-//   const getCourses = () => {
-//     // const id = person._id;
-//     axios
-//     .get(`${process.env.REACT_APP_URL}/educator/mycourses/${person._id}`, {
-//       headers: authHeader(),
-//     })
-//     .then((response) => {
-//       console.log(response);
-//       // alert(response.data.message);
-//       // navigate("/mycourses");
-//     })
-//     .catch((response) => {
-//       console.log(response.response.data.message);
-//     });
-//   }
-//   useEffect(() => {
-//        getCourses();
-//   },[])
-
+  const allCourses = async () => {
+    try {
+      axios
+        .get(`${process.env.REACT_APP_URL}/courses`, {
+          headers: authHeader(),
+        })
+        .then((response) => {
+          setCourse(response.data);
+        })
+        .catch((response) => {
+          alert(response.response.data.message);
+        });
+    } catch (err) {
+      console.log(err);
+      navigate("/");
+    }
+  };
 
 
   return (
@@ -42,18 +38,22 @@ const CourseList = () => {
         <h1 className="page-title">All courses</h1>
         <div className="page-content">
           <div className="row grid2">
-            <SingleCourse
-              title="Course 1"
-              educator="Aaryan Khandelwal"
-              date="06-10-2022"
-            >
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Reprehenderit nemo atque itaque perferendis at vitae consequuntur,
-              magni inventore facere architecto voluptas illum ratione ut soluta
-              enim. Autem nam blanditiis quod!
-            </SingleCourse>
-            <SingleCourse title="Course 2"></SingleCourse>
-          </div>
+            {course.map((item, index) => {
+              return (
+                <>
+                  <SingleCourse
+                    title={item.title}
+                    id={item._id}
+                    educator={item.educatorName}
+                    date="06-10-2022"
+                    category={item.educatorName}
+                  >
+                    {item.description}
+                  </SingleCourse>
+                </>
+              );
+            })}
+         </div>
         </div>
       </main>
     </>
